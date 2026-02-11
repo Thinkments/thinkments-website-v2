f0af68a623e4a40d2bd100a319abab9e810a454f
Corey Spicer <Spicer@thinkments.com>
2026-02-10 19:23:08 -0600

    chore: Remove temporary blog migration and debugging utility scripts.

--- debug-seo.js
+++
@@ -1,8 +0,0 @@
-try {
-  console.log('Attempting to run generateStaticPages.js');
-  const result = require('./src/scripts/generateStaticPages.js');
-  console.log('Completed require');
-} catch (e) {
-  console.error('CAUGHT ERROR:');
-  console.error(e);
-}
